fe1f415ee8923e3a1e4ff12466079df17802706b
D <haydend4@gmail.com>
2020-04-21 16:00:09 -0700

    add and traversals-displays work

--- BTreeTraversal.cpp
+++ BTreeTraversal.cpp
@@ -4,25 +4,34 @@
 
 using namespace std;
 
-void visit( int intVal );
+void displayInt( int& intVal );
 
 int main()
 {
-	BinarySearchTree<int> bTree(5);
+	BinarySearchTree<int> bTree(14);
 	cout<<bTree.getRootData()<<endl;
 	cout<<"count: "<<bTree.getNumberOfNodes()<<endl;
 	bTree.add(7);
-	bTree.add(14);
+	bTree.add(5);
 	bTree.add(21);
 	bTree.add(9);
 	cout<<"count: "<<bTree.getNumberOfNodes()<<endl;
 
-	bTree.inorderTraverse(visit);//displayInt);
+	cout<<"preorder: ";
+	bTree.preorderTraverse(displayInt);
+	cout<<endl;
+
+	cout<<"inorder: ";
+	bTree.inorderTraverse(displayInt);
+	cout<<endl;
+
+	cout<<"postorder: ";
+	bTree.postorderTraverse(displayInt);
 	cout<<endl;
 	return 0;
 }
 
-void visit( int intVal )
+void displayInt( int& intVal )
 {
 	cout<<intVal<<',';
 }
